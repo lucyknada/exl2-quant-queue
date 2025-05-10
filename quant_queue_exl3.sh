03378efff513c540c6316c5ade29167f917998ed
lucy <154630366+lucyknada@users.noreply.github.com>
2025-05-10 14:16:49 +0200

    Create quant_queue_exl3.sh

---
+++ quant_queue_exl3.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+USER=""
+
+quant() {
+  BPW="$2"
+
+  WORK_DIRECTORY="./output/$1/temp"
+  OUTPUT_DIRECTORY="./output/$1/output-${BPW}"
+
+  mkdir -p "$WORK_DIRECTORY" || exit
+  mkdir -p "$OUTPUT_DIRECTORY" || exit
+
+  if [ -f "${OUTPUT_DIRECTORY}/config.json" ]; then
+    echo "Conversion for $1 at ${BPW}bpw already complete. Skipping."
+    continue
+  fi
+
+  if [ -f "${WORK_DIRECTORY}/job.json" ]; then
+    echo "Resuming conversion for $1 at ${BPW}bpw."
+    python convert.py \
+      -d 0,1 \
+      -w "$WORK_DIRECTORY" \
+      -r || exit
+  else
+    echo "Starting new conversion for $1 at ${BPW}bpw."
+    python convert.py \
+      -i ./models/$1/ \
+      -w "$WORK_DIRECTORY" \
+      -d 0,1 \
+      -o "$OUTPUT_DIRECTORY" \
+      -b $BPW || exit
+  fi
+
+  cat "./models/${1}/README.md" | sed -z "s/---/---\n### exl3 quant\n---\n### check revisions for quants\n---\n/2" > "${OUTPUT_DIRECTORY}/README.md"
+  HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli upload --private --revision ${BPW}bpw "${USER}/${1}-exl3" "${OUTPUT_DIRECTORY}" || return 0
+  HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli upload --private "${USER}/${1}-exl3" ${OUTPUT_DIRECTORY}/README.md ./README.md || return 0
+}
+
+while IFS= read -r line || [[ -n "$line" ]]; do
+  modified_line="${line#https://}"
+  IFS="/" read -ra parts <<< "$modified_line"
+  x="${parts[-3]}"
+  y="${parts[-2]}"
+  BPW="${parts[-1]}"
+
+  if [ ! -f "./models/${x}_${y}/config.json" ]; then
+    HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download "${x}/${y}" --exclude "*checkpoint*" "*global_state*" "model.safetensors" "*.arrow" "*.pth" "*.pt" "*.nemo" --local-dir="./models/${x}_${y}"
+    if ls ./models/${x}_${y}/*.bin 1> /dev/null 2>&1; then
+      python ./util/convert_safetensors.py ./models/${x}_${y}/*.bin
+      rm ./models/${x}_${y}/*.bin
+    fi
+    if ls ./models/${x}_${y}/*.pth 1> /dev/null 2>&1; then
+      rm ./models/${x}_${y}/*.pth
+    fi
+  fi
+
+  quant "${x}_${y}" "$BPW"
+done < "queue.txt"
